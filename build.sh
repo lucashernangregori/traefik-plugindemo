307f5740334d1d0dcacca76ba9a671a2f04cb04c
lgregori <lgregori@gmail.com>
2021-01-03 01:07:49 -0300

    adding docker run

--- build.sh
+++ build.sh
@@ -2,3 +2,12 @@
 apt update
 apt install -y docker.io
 docker build -t traefik_test:1.0 .
+
+mkdir log
+docker run -d -p 8080:8080 -p 80:80 \
+-v $PWD/traefik/traefik.yml:/etc/traefik/traefik.yml \
+-v $PWD/traefik/dynamic.yml:/etc/traefik/dynamic.yml \
+-v $PWD/log:/var/log \
+-v /var/run/docker.sock:/var/run/docker.sock \
+--name traefikt \
+traefik_test:1.0
--- dockerfile
+++ dockerfile
@@ -1,3 +1,2 @@
 FROM traefik:v2.3.6
-COPY ./traefik /home/ubuntu/traefik
 COPY ./plugin /home/ubuntu/traefik/plugins/src/plugindemo
